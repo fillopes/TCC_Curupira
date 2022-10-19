8c1d230944e15bd74f0a2dae5d1a24f95e5328dc
Filipe Lopes <filipe.glopes@hotmail.com>
2022-10-18 23:25:43 -0300

    Adicionado mensagens de aviso para ssl e tls

--- curupira.py
+++ curupira.py
@@ -2,6 +2,16 @@ import subprocess, sys, re
 
 output = subprocess.check_output(["sslscan", "--no-colour", "https://google.com"])
 outputd = output.decode()
-print(outputd)
-match = re.findall(r'TLSv1\.1\s\s\senabled', outputd,flags=re.I|re.M)
-print (match)
+#print(outputd)
+match = re.findall(r'SSLv2 +enabled', outputd,flags=re.I|re.M)
+if match != []:
+    print ("Problema com SSLv2 ativado")
+match = re.findall(r'SSLv3 +enabled', outputd,flags=re.I|re.M)
+if match != []:
+    print ("Problema com SSLv3 ativado")
+match = re.findall(r'TLSv1\.0 +enabled', outputd,flags=re.I|re.M)
+if match != []:
+    print ("Problema com TLSv1.0 ativado")
+match = re.findall(r'TLSv1\.1 +enabled', outputd,flags=re.I|re.M)
+if match != []:
+    print ("Problema com TLSv1.1 ativado")
