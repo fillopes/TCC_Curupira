841f9df13606e17d07eace1654e16b42bf3396a7
Filipe Lopes <filipe.glopes@hotmail.com>
2022-10-19 22:59:26 -0300

    Adicionado opções de ler args

--- curupira.py
+++ curupira.py
@@ -1,6 +1,22 @@
-import subprocess, sys, re
+import subprocess, sys, re, getopt
 
-output = subprocess.check_output(["sslscan", "--no-colour", "https://google.com"])
+
+host =""
+directory =""
+argv = sys.argv[1:]
+try:
+    options, args = getopt.getopt(argv, "h:d:", ["host =", "directory ="])
+except:
+    print("Error Message ")
+ 
+for name, value in options:
+    if name in ['-h', '--host']:
+        host = value
+    elif name in ['-d', '--directory']:
+        directory = value
+
+
+output = subprocess.check_output(["sslscan", "--no-colour", host])
 outputd = output.decode()
 #print(outputd)
 match = re.findall(r'SSLv2 +enabled', outputd,flags=re.I|re.M)
