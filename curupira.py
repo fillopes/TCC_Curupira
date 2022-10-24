c8abc18fef7b0385fed070c5bcf5cb12d6c575a3
Filipe Lopes <filipe.glopes@hotmail.com>
2022-10-24 20:36:33 -0300

    Refactoramento do nome de variáveis

--- curupira.py
+++ curupira.py
@@ -1,34 +1,34 @@
 import subprocess, sys, re, getopt
 
-
-host =""
-directory =""
+host = ""
+directory = ""
 argv = sys.argv[1:]
 try:
     options, args = getopt.getopt(argv, "h:d:", ["host =", "directory ="])
 except:
     print("Error Message ")
- 
+
 for name, value in options:
     if name in ['-h', '--host']:
         host = value
     elif name in ['-d', '--directory']:
         directory = value
 
-
 sslscan_output = subprocess.check_output(["sslscan", "--no-colour", host])
 sslscan_outputd = sslscan_output.decode()
-#print(outputd)
-match = re.findall(r'SSLv2 +enabled', sslscan_outputd,flags=re.I|re.M)
-if match != []:
-    print ("Problema com SSLv2 ativado")
-match = re.findall(r'SSLv3 +enabled', sslscan_outputd,flags=re.I|re.M)
-if match != []:
-    print ("Problema com SSLv3 ativado")
-match = re.findall(r'TLSv1\.0 +enabled', sslscan_outputd,flags=re.I|re.M)
-if match != []:
-    print ("Problema com TLSv1.0 ativado")
-match = re.findall(r'TLSv1\.1 +enabled', sslscan_outputd,flags=re.I|re.M)
-if match != []:
-    print ("Problema com TLSv1.1 ativado")
 
+matchSSLv2 = re.findall(r'SSLv2 +enabled', sslscan_outputd, flags=re.I | re.M)
+if matchSSLv2 != []:
+    print("Problema com SSLv2 ativado")
+
+matchSSLv3 = re.findall(r'SSLv3 +enabled', sslscan_outputd, flags=re.I | re.M)
+if matchSSLv3 != []:
+    print("Problema com SSLv3 ativado")
+
+matchTLS10 = re.findall(r'TLSv1\.0 +enabled', sslscan_outputd, flags=re.I | re.M)
+if matchTLS10 != []:
+    print("Problema com TLSv1.0 ativado")
+
+matchTLS11 = re.findall(r'TLSv1\.1 +enabled', sslscan_outputd, flags=re.I | re.M)
+if matchTLS11 != []:
+    print("Problema com TLSv1.1 ativado")
